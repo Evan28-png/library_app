1680c311a2425321c14950faf00ded54297b229f
Evan Pierre <evanpierre23@gmail.com>
2025-05-23 07:34:43 +0300

    fixed exec command error

--- wait_for_mysql.sh
+++ wait_for_mysql.sh
@@ -4,7 +4,8 @@ while ! nc -z db 3306; do
   sleep 1.4
 done
 echo "MySQL is up!"
-exec flask db upgrade 
-exec gunicorn app:app --bind 0.0.0.0:3000
+flask db upgrade 
+gunicorn app:app --bind 0.0.0.0:3000 --log-level debug --capture-output
 
-
+#use exec only once pref last command to run
+#as it repleacves the shell
